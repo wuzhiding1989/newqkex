a7d9895d81ba68499c7eab8ddc7caac332871160
254160096@qq.com <254160096@qq.com>
2023-05-11 18:28:35 +0800

    123

--- BU/futures/api/webapi.py
+++ BU/futures/api/webapi.py
@@ -1,6 +1,7 @@
 import copy
 import requests
-
+symbol='BTCUSDT';tradeType='linearPerpetual';side='buy';marginType='cross';positionSide='positionSide'
+postOnly='false';reduceOnly='false';orderType='limit';priceType='optimalN';pageNum='1';pageSize='10'
 headers = {"Content-Type": "application/json", "Accept-Language": "zh-CN", "source": "web", "X-Authorization": ""}
 Authorization = 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIyN2MwNjE5Zi04N2FlLTQ4ODEtYjFkMi1lODFlZGZjNzcxZmEiLCJ1aWQiOiJXWFAxUS8xa2s5NVQxTjRxOWxuSFRBPT0iLCJiaWQiOiJXWFAxUS8xa2s5NVQxTjRxOWxuSFRBPT0iLCJpcCI6IkdwdHl4M01ZbzBJemNsL3pwN0ZiNXc9PSIsImRldiI6InAva3BIckF3RkJjSUZleXg0U2xkZGc9PSIsInN0cyI6MCwiaWF0IjoxNjcyNTAyNDAwLCJleHAiOjE2ODgxNDA4MDAsImlzcyI6InFrZXgifQ.7HKuzZz-IC0_Zs5hVK420jVbgpsgRP-NlYtxrUiTs0U'
 Authorization1 = 'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJlNTk4ZDBiOS1lOTcyLTQ1N2MtOWRmOS1lMTAyOGQ2MmM1Y2YxOTkyNDEyMzQzIiwidWlkIjoiaDBsVXZiR0t2SkdkdGVscGYxQWRZUT09IiwiYmlkIjoibVdPTzdGMnpzTjBUd1JBeVFEbGsrQT09IiwiaXAiOiJkRmxJM3RwSFdJdHpsNk9rTDRBSlBRPT0iLCJkZXYiOiJBOG9MTmVSVnZGR294TDlQWmVoa3BBPT0iLCJzdHMiOjAsImlhdCI6MTY4Mzc5MjQ0NiwiZXhwIjoxNjgzODc4ODQ2LCJpc3MiOiJ3Y3MifQ.IErDpB3ydg6mbOxbq6TlMA-0quPgzx6ep2uNkE0Q7nU'
@@ -86,11 +87,11 @@ def futures_openOrders(tradeType=None,symbol=None,side=None,clOrdId=None,orderId
         "clOrdId":clOrdId,
         "pageNum": pageNum,
         "pageSize": pageSize}
-    res = requests.post(url=tradeurl + path, json=params, headers=headers).json()
+    res = requests.get(url=tradeurl + path, params=params, headers=headers).json()
     return res
 
 
 if __name__ == '__main__':
-    print(futures_transfer(fromAccountType='funding', toAccountType='futures', currency='USDT', amount='200'))
-    # print(futures_order())
-    print(futures_position())
+    #print(futures_transfer(fromAccountType='funding', toAccountType='futures', currency='USDT', amount='200'))
+    print(futures_openOrders(tradeType=tradeType,symbol=symbol))
+    print(futures_position(tradeType,symbol,marginType))
