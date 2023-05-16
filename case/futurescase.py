3ec75288e30878b35dafb11defc1aa72666100aa
254160096@qq.com <254160096@qq.com>
2023-05-16 17:12:27 +0800

    调试合约接口

--- case/futurescase.py
+++ case/futurescase.py
@@ -10,22 +10,24 @@ def order_ad():
     '''一键平仓、撤单
     当前资金、订单、持仓查询
     历史资金、订单、持仓查询'''
-    # tradingAccount=wb.web_tradingAccount()
-    # print(tradingAccount)
-    # available=wb.web_transfer(fromAccountType=fromAccountType,toAccountType=toAccountType,currency=currency,amount=amount)
-    # print(available)
-    # se=wb.web_order(tradeType=tradeType, symbol=symbol, side=side, positionSide=positionSide, orderType=orderType, reduceOnly=reduceOnly,
-    #               marginType=marginType, price='20000', priceType=priceType, orderQty=1, postOnly=postOnly, timeInForce=timeInForce)
-    # print(se)
-    # w=wb.web_openOrders(tradeType=tradeType) #当前委托
-    # id=w['data']['list'][0]['orderId']
-    # print(w)
-    # print(id)
-    # cl=wb.web_orders_cancel(tradeType=tradeType,orderId=id,symbol=symbol)#撤单
-    # print(cl)
-    print(wb.web_position(tradeType, symbol, marginType))
-    ac=ut.login_email('y005@cc.com','q123456')
-    print(ac)
+    user = wb.webapi(2, 'test')
+    #tradingAccount=user.web_tradingAccount()#划转域名需要检查
+    print(user.web_position(tradeType, symbol, marginType))
+    #print(user.web_tradingAccount())
+    #print(tradingAccount)
+    available=user.web_transfer(fromAccountType=fromAccountType,toAccountType=toAccountType,currency=currency,amount=amount)
+    print(available)
+    se=user.web_order(tradeType=tradeType, symbol=symbol, side=side, positionSide=positionSide, orderType=orderType, reduceOnly=reduceOnly,
+                  marginType=marginType, price='20000', priceType=priceType, orderQty=1, postOnly=postOnly, timeInForce=timeInForce)
+    print(se)
+    w=user.web_openOrders(tradeType=tradeType) #当前委托
+    id=w['data']['list'][0]['orderId']
+    print(w)
+    print(id)
+    cl=user.web_orders_cancel(tradeType=tradeType,orderId=id,symbol=symbol)#撤单
+    print(cl)
+    # user=wb.webapi(2,'test')
+
 
 if __name__ == '__main__':
     print(order_ad())
