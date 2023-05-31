1dff74db0733ee31ffcde23121bd2aad6ce1f90f
254160096@qq.com <254160096@qq.com>
2023-05-31 10:30:58 +0800

    封装直接给用户加钱的方法

--- BU/futures/testcase/wsstestfutures.py
+++ BU/futures/testcase/wsstestfutures.py
@@ -1,34 +1,62 @@
-import websockets.client
+import json,ssl
 import websocket
+#from WsDataDTO_pb2 import WsKlineDTO
+
+# 创建 WebSocket 连接
+ws_url = "wss://ws-socket.qkex.com/v1/market"
+subscribe_index = {"event":"subscribe","data":[{"tradeType":"linearPerpetual","symbol":"BTCUSDT","stream":"kline1m"}]}
+
+import asyncio
+import websockets
+import json
+
+
+import asyncio
+import websockets
 import json
 import ssl
 
-def on_message(ws, message):
-    data = json.loads(message)
-def on_error(ws, error):
-    print(error)
-def on_close(ws, *args):
-    print("### closed ###")
+ssl_context = ssl.SSLContext()
+ssl_context.verify_mode = ssl.CERT_NONE
 
-def on_open(ws):
-    print("### connected ###")
-    subscribe_index = {"event":"subscribe","data":[{"tradeType":"linearPerpetual","symbol":"BTCUSDT","stream":"index"}]}
-    subscribe_ticker24hr = {"event":"subscribe","data":[{"tradeType":"linearPerpetual","symbol":"BTCUSDT","stream":"ticker24hr"}]}
-    subscribe_trade = {"event": "subscribe", "data": [{"tradeType": "linearPerpetual", "symbol": "BTCUSDT", "stream": "trade"}]}
-    subscribe_depth0 = {"event":"subscribe","data":[{"tradeType":"linearPerpetual","symbol":"BTCUSDT","stream":"depth0"}]}
-    ws.send(json.dumps(subscribe_index))
-    # ws.send(json.dumps(subscribe_ticker24hr))
-    # ws.send(json.dumps(subscribe_trade))
-    # ws.send(json.dumps(subscribe_depth0))
+async def connect():
+    async with websockets.connect(ws_url, ssl=ssl_context) as websocket:
+        await websocket.send(json.dumps(subscribe_index))
+        while True:
+            response = await websocket.recv()
+            response_dict = json.loads(response)
+            if 'data' in response_dict:
+                data = response_dict['data']
+                for item in data:
+                    if 'stream' in item and item['stream'] == 'kline1m':
+                        kline_data = item['data']
+                        for kline in kline_data:
+                            print(f"Period: {kline['p']}")
+                            print(f"Open Time: {kline['ot']}")
+                            print(f"Close Time: {kline['ct']}")
+                            print(f"Open Price: {kline['op']}")
+                            print(f"Close Price: {kline['cp']}")
+                            print(f"High Price: {kline['h']}")
+                            print(f"Low Price: {kline['l']}")
+                            print(f"Volume: {kline['v']}")
+                            print(f"Quote Value: {kline['q']}")
+                            print(f"Count: {kline['c']}")
+                            print(f"Price Change: {kline['pc']}")
+                            print(f"Price Change Percent: {kline['pcp']}")
+                            print("\n")
+
+asyncio.get_event_loop().run_until_complete(connect())
 
 
-if __name__ == "__main__":
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("wss://ws-socket.qkex.com/v1/market",
-                                on_message=on_message,
-                                on_error=on_error,
-                                on_close=on_close)
-    ws.on_open = on_open
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
+
+# ws = websocket.WebSocketApp(ws_url,
+#                             on_open=on_open,
+#                             on_message=on_message,
+#                             on_error=lambda ws, error: print(error),
+#                             on_close=lambda ws: print("WebSocket closed"))
+
+# 运行 WebSocket 连接
+# ws.run_forever()
+# ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
 
--- common/mysql_san.py
+++ common/mysql_san.py
@@ -21,10 +21,32 @@ def sql_send(sql):
     for tmp in res:
         id.append(tmp[0])
     return id
-
+def mysql_execute(sql):#修改数据库内容或拆入数据
+    db = pymysql.connect(host=host, user=user, password=password, database="")
+    cursor = db.cursor()
+    try:
+        cursor.execute(sql)
+        db.commit()
+        print("执行成功")
+    except:
+        db.rollback()
+        print("执行失败")
+    db.close()
+def add_account(uid,currency,balance):#给钱包价钱，加到钱包账户
+    sql_select = f"SELECT balance FROM wallet.user_balance WHERE user_id in ({uid}) AND parent_symbol='{currency}' AND currency_id=0"
+    install_select = f"INSERT INTO wallet.user_balance ( `currency_id`, `parent_symbol`, `user_id`, `balance`, `create_on`, `update_on`) VALUES ( 0, '{currency}', {uid}, {balance}, '2023-02-22 11:08:19', '2023-02-25 14:30:41')"
+    updata_select = f"UPDATE wallet.user_balance SET balance = '{balance}' WHERE parent_symbol ='{currency}' AND currency_id=0 AND user_id in ({uid})"
+    abc=mysql_select(sql_select)
+    if len(abc)==0:#判断钱包是否有数据
+        a=mysql_execute(install_select)
+        print(a,install_select)
+    else:
+        a=mysql_execute(updata_select)
+        print(updata_select,a)
 
 if __name__ == '__main__':
-    user_id=10122165; legal_symbol='usd'; symbol='btc'
-    sql = f"SELECT a.fee_rate,b.`status`, b.ratio,b.target_uid AS s_uid ,b.source_uid as f_uid,(SELECT platform_commission_rate FROM OTC.config_currency WHERE symbol='{symbol}' AND legal_symbol='{legal_symbol}') AS fee FROM OTC.user_info a,OTC.rebate_config b WHERE a.user_id=b.source_uid AND a.user_id in ({user_id})"
-    a = mysql_select(sql)
-    print(a)
+    # user_id=10122165; legal_symbol='usd'; symbol='btc'
+    # sql = f"SELECT a.fee_rate,b.`status`, b.ratio,b.target_uid AS s_uid ,b.source_uid as f_uid,(SELECT platform_commission_rate FROM OTC.config_currency WHERE symbol='{symbol}' AND legal_symbol='{legal_symbol}') AS fee FROM OTC.user_info a,OTC.rebate_config b WHERE a.user_id=b.source_uid AND a.user_id in ({user_id})"
+    # a = mysql_select(sql)
+    # print(a)
+    print(add_account(uid='10122167',currency="USDT",balance='7000000.998'))
